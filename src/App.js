3146223fe6aa54b2d792448a9a03d94b855d6358
Guillermo <gyanezfeliu@gmail.com>
2020-05-04 11:50:30 -0400

    alerts system working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import BaseRouter from './routes';
+import Alerts from './components/Layout/Alerts';
 import './App.scss';
+
+// Alert Options
+const alertOptions = {
+  timeout: 3000,
+  position: 'top center'
+}
 
 function App() {
 
   return (
     <div className="App">
-      <BaseRouter />
+        <Alerts />
+        <BaseRouter />
     </div>
   );
 }
